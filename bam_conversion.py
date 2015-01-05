cb108b434e96ead7fe0b88654c4fe4867453e2a9
Patrick LOmbard <pdl30@cam.ac.uk>
2015-01-05 14:39:56 +0000

    fixed names

--- bam_conversion.py
+++ bam_conversion.py
@@ -16,7 +16,8 @@ import ConfigParser
 import itertools
 
 def sam_to_bam(sam):
-	name = sam.strip(".sam")
+	name = sam.rstrip("$.sam")
+	print name
 	command1 = "samtools view -bS {} > {}.bam".format(sam, name)
 	command2 = "samtools sort {0}.bam {0}_sort".format(name)
 	command3 = "samtools index {}_sort.bam".format(name)
